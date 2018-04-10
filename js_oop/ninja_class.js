7077d8c1a7f3a3b8458455fa7f08146f5ff1510e
Corey Cunningham <coreyjjc@me.com>
2018-04-10 09:07:27 -0400

    ninja class III

--- js_oop/ninja_class.js
+++ js_oop/ninja_class.js
@@ -1,41 +1,82 @@
-function Ninja(name,health){
-  this.name = name;
-  this.health = 100;
-  const speed = 3;
-  const strength = 3;
-  this.sayName = function(){
+// function Ninja(name,health){
+//   this.name = name;
+//   this.health = 100;
+//   const speed = 3;
+//   const strength = 3;
+//   this.sayName = function(){
+//     console.log(this.name);
+//   }
+//   this.showStats = function(){
+//     console.log("Name: " + this.name + ", Strength: " + strength + ", Speed: " + speed);
+//   }
+//   this.drinkSake = function(){
+//     this.health += 10;
+//   }
+//
+//   this.punch = function(ninja){
+//     if (ninja instanceof Ninja){
+//       ninja.subtractHealth(5);
+//       console.log(ninja.name + " was punched by " + this.name + " and lost 5 health!");
+//     }
+//     else {
+//       console.log("ERROR");
+//     }
+//   }
+//
+//   this.kick = function(ninja){
+//     if (ninja instanceof Ninja){
+//       ninja.subtractHealth(15);
+//       console.log(ninja.name + " was kicked by " + this.name + " and lost 15 health!");
+//     }
+//     else {
+//       console.log("ERROR");
+//     }
+//   }
+//
+//   this.subtractHealth = function(num){
+//     this.health -= num;
+//   }
+// }
+
+class Ninja {
+  constructor(name){
+    this.name = name;
+    this.health = 100;
+    this.strength = 3;
+    this.speed = 3;
+  }
+
+  showStats() {
+    console.log("Name: " + this.name + ", Strength: " + this.strength + ", Speed: " + this.speed);
+  }
+
+  sayName() {
     console.log(this.name);
   }
-  this.showStats = function(){
-    console.log("Name: " + this.name + ", Strength: " + strength + ", Speed: " + speed);
-  }
-  this.drinkSake = function(){
-    this.health += 10;
-  }
 
-  this.punch = function(ninja){
+  punch(ninja) {
     if (ninja instanceof Ninja){
-      ninja.subtractHealth(5);
-      console.log(ninja.name + " was punched by " + this.name + " and lost 5 health!");
-    }
-    else {
-      console.log("ERROR");
-    }
+         ninja.subtractHealth(5);
+         console.log(ninja.name + " was punched by " + this.name + " and lost 5 health!");
+       }
+       else {
+         console.log("ERROR");
+       }
   }
 
-  this.kick = function(ninja){
-    if (ninja instanceof Ninja){
-      ninja.subtractHealth(15);
-      console.log(ninja.name + " was kicked by " + this.name + " and lost 15 health!");
+  kick(ninja){
+      if (ninja instanceof Ninja){
+        ninja.subtractHealth(15);
+        console.log(ninja.name + " was kicked by " + this.name + " and lost 15 health!");
+      }
+      else {
+        console.log("ERROR");
+      }
     }
-    else {
-      console.log("ERROR");
-    }
-  }
 
-  this.subtractHealth = function(num){
-    this.health -= num;
-  }
+    subtractHealth(num){
+      this.health -= num;
+    }
 }
 
 const ninja1 = new Ninja("Hyabusa");
@@ -43,3 +84,18 @@ const ninja2 = new Ninja("Joe");
 ninja1.sayName();
 ninja1.showStats();
 ninja2.kick(ninja1);
+
+class Sensei extends Ninja {
+  constructor(name){
+    super(name)
+    this.health = 100;
+    this.strength = 10;
+    this.speed = 10;
+  }
+
+  speakWisdown(){
+    console.log("What one programmer can do in one month, two programmers can do in two months.")
+  }
+}
+const sens = new Sensei("Master Splinter");
+sens.showStats();
