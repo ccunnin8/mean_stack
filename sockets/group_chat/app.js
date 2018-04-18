50a08c402b4eb9533a001fbab879ee994ad9c8e0
Corey Cunningham <coreyjjc@me.com>
2018-04-18 16:16:59 -0400

    group chat

--- sockets/group_chat/app.js
+++ sockets/group_chat/app.js
@@ -2,13 +2,18 @@ var express = require('express');
 var session = require('express-session');
 var socket = require('socket.io');
 var app = express();
+var chatlog = {
+  "users": {},
+  "messages": []
+}
 
 app.set("view engine", "ejs");
 
 app.use(express.static(__dirname + "/static"));
+app.use(session({secret: "elSecreto"}));
 
 app.get("/",(req,res)=>{
-  res.render("index");
+  res.render("index", {"messages": chatlog["messages"] });
 });
 
 var server = app.listen(8000,()=>{
@@ -21,12 +26,26 @@ io.sockets.on("connection",function(socket){
   //listen for new user has joined
     //store new user in session ?
     //broadcast new user joined message
+  socket.on("new_user",(data)=>{
+    chatlog.messages.push(`${data.name} has joined the chat`);
+    chatlog.users[socket.id] = data.name;
+    socket.broadcast.emit("new_user_server",(data));
+  });
 
   //listen for  new user has sent message
     //store new user message in session.user.messages.push(message) ?
     //broadcast user + message
+  socket.on("new_message",(data)=>{
+    chatlog.messages.push(`${data.name}: ${data.message}`);
+    socket.broadcast.emit("new_message_server", data);
+  });
+
 
   //listen for user has left
      //delete user from session?
      //broadcast user has left
+  socket.on("disconnect",()=>{
+    socket.broadcast.emit("user_left",{name: chatlog.users[socket.id]});
+    delete chatlog.users[socket.id];
+  });
 });
--- sockets/group_chat/static/main.js
+++ sockets/group_chat/static/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   var new_user = prompt("Welcome to the chatboard, what is your name?");
   var chat_box = $(".messages");
   var socket = io.connect();
-  //local actions
+
   add_new_user(new_user);
 
   $("button").on("click",add_new_message);
@@ -20,21 +20,32 @@ $(document).ready(function(){
     chat_box.append(msg);
   });
 
+  socket.on("user_left",function(data){
+    var msg = new_message(data.name + " has left the chat");
+    chat_box.append(msg);
+  });
   //helper functions
   function new_message(text){
     return ("<p class='message'>" + text + "</p>");
   };
 
+  function clear_text(){
+    $("input").val("");
+  }
   //functions to handle actions
   function add_new_user(){
     var msg = new_message(new_user + " has joined the chat!");
     chat_box.append(msg);
+    clear_text();
     socket.emit("new_user",{ name: new_user });
   }
 
   function add_new_message(){
+    console.log("SENT A NEW MESSAGE");
     var msg = new_message(new_user + ": " + $("input").val());
+    var text = $("input").val()
     chat_box.append(msg);
+    clear_text();
     socket.emit("new_message", { name: new_user, message: text })
   }
 });
