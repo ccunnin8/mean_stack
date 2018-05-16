e608a4fe95b1ba8c148f4e2624e9ae7f57985a8c
Corey Cunningham <coreyjjc@me.com>
2018-05-16 15:22:38 -0400

    started product manager

---
+++ angular/product-mgmt/src/app/product-list/product-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input} from '@angular/core';
+import { ProductsService } from '../products.service';
+import { Product } from '../product';
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products: Product[];
+
+  constructor(private _products: ProductsService) { }
+
+  ngOnInit() {
+    this.products = this._products.getProducts();
+  }
+
+}
